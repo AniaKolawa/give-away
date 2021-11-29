760333f6cfc5da6d40c20b1a6393b723c5dcc893
Ania Kolawa <ak7koron@gmail.com>
2021-11-29 16:01:05 +0100

    contact test logic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import decoration from "../assets/Decoration.svg";
 import facebook from "../assets/Facebook.svg";
 import instagram from "../assets/Instagram.svg";
 
+import useForm from "./useForm";
+import validate from "./validateInfo";
 
 const Contact = ({name}) => {
+
+//     useEffect(() => {
+//
+//
+//
+//     //     fetch(`https://fer-api.coderslab.pl/v1/portfolio/contact`, {
+//     //         method: "POST",
+//     //         body: JSON.stringify(form),
+//     //         headers: {
+//     //             "Content-Type": "application/json"
+//     //         }
+//     //     })
+//     //         .then(response => response.json())
+//     //         .then(data => {
+//     //             console.log(data);
+//     //         })
+//     //         .catch(error => {
+//     //             console.log(error);
+//     //         });
+//     }, []);
+const {handleChange, values, handleSubmit, errors} = useForm(validate);
+
     return (
         <section id={name} className="contact">
+            {/*{console.log(form)}*/}
             <div className="container">
                 <div className="contact__contentContainer">
                     <h2 className="title">Skontaktuj się z nami</h2>
                     <div className="decoration"><img alt="decoration" src={decoration}/></div>
-                    <form className="contact__form">
+                    <form onSubmit={handleSubmit} className="contact__form">
                         <div className="form__inputs">
                             <div className="form__item">
                                 <label className="form__label" htmlFor="name">Wpisz swoje imię</label>
@@ -20,20 +45,20 @@ const Contact = ({name}) => {
                                        name="name"
                                        type="text"
                                        placeholder="Krzysztof"
-                                    // value={}
-                                    // onChange={e => setXXX(e.target.value)}
-                                />
+                                       value={values.name}
+                                       onChange={handleChange}/>
+                                {errors.name && <div className="error">{errors.name}</div>}
                             </div>
                             <div className="form__item">
-                                <label className="form__label" htmlFor="mail">Wpisz swój email</label>
+                                <label className="form__label" htmlFor="email">Wpisz swój email</label>
                                 <input className="form__input"
-                                       id="mail"
-                                       name="mail"
+                                       id="email"
+                                       name="email"
                                        type="text"
                                        placeholder="abc@xyz.pl"
-                                    // value={}
-                                    // onChange={e => setXXX(e.target.value)}
-                                />
+                                       value={values.email}
+                                       onChange={handleChange}/>
+                                {errors.email && <div className="error">{errors.email}</div>}
                             </div>
                         </div>
 
@@ -43,12 +68,12 @@ const Contact = ({name}) => {
                                       name="message"
                                       id="message"
                                       rows="4"
-                                // value={message}
+                                      value={values.message}
                                       placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                                // onChange={e => setXXX(e.target.value)}
+                                      onChange={handleChange}
                             />
+                            {errors.message && <div className="error">{errors.message}</div>}
                         </div>
-
                     </form>
                     <button className="form__button button" type="submit">Wyślij</button>
                 </div>
---
+++ src/components/validateInfo.js
@@ -0,0 +1,21 @@
+export default function validateInfo(values) {
+    let errors = {};
+
+    if(!values.name){
+        errors.name = "Podaj imię"
+    } else if (values.name.includes(" ")){
+        errors.name = "Podane imię jest nieprawidłowe!"
+    }
+
+    if(!values.email) {
+        errors.email = "Podaj email"
+    } else if (!/\S+@\S+\.\S+/.test(values.email)){
+        errors.email = "Podany email jest nieprawidłowy!"
+    }
+
+    if(values.message.length < 120){
+        errors.message = "Wiadomość nie może być krótsza niż 120 znaków"
+    }
+
+    return errors;
+}
