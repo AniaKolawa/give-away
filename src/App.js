3be0fc654df206a45ba13962bada5f10ce62bfdc
Ania Kolawa <ak7koron@gmail.com>
2021-12-02 19:32:06 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
 import './App.css';
-import {BrowserRouter as Router,
-  Switch,
-  Route,
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
 } from "react-router-dom";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
@@ -11,27 +11,19 @@ import Register from "./components/Register";
 import Logout from "./components/Logout";
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Menu/>
-          <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route path="/login">
-                <Login />
-            </Route>
-              <Route path="/rejestracja">
-                  <Register />
-              </Route>
-              <Route path="/wylogowano">
-                  <Logout />
-              </Route>
-          </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Menu/>
+                <Switch>
+                    <Route exact path="/" component={props => <Home {...props} /> } />
+                    <Route path="/login"  component={props => <Login {...props} />} />
+                    <Route path="/rejestracja" component={props => <Register {...props} />} />
+                    <Route path="/wylogowano" component={props => <Logout {...props} /> } />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/LowerMenu.js
+++ src/components/LowerMenu.js
@@ -1,37 +1,36 @@
-import React, {useEffect, useState} from 'react';
-import {Link as LinkScroll} from "react-scroll";
-import {Link} from "react-router-dom"
+import React, {useEffect} from 'react';
+import { withRouter } from "react-router-dom"
 
 
 
-const LowerMenu = () => {
-
-const [pathname, setPathname] = useState(window.location.pathname)
+const LowerMenu = (props) => {
 
     useEffect(() => {
-        setPathname(window.location.pathname)
-    }, [window.location.pathname])
+        const location = props.location?.state?.location
+        if(location) scrollTo(location)
+    }, [props.location])
+
+    const scrollTo = (location)=>{
+        const element = document.getElementById(location);
+        window.scrollTo({
+            behavior: "smooth",
+            top: element ? element.offsetTop : 0
+        });
+    }
+
+    const changeLocation = (param = null) =>{
+        props.history.push('/',{location:param})
+    }
 
     return (
         <div className="lowerNav-container">
-            {pathname === "/" ?
-                <>
-                    <LinkScroll className="lowerMenuLink lowerMenuLink--active" to="/">Start</LinkScroll>
-                    <LinkScroll smooth={true} className="lowerMenuLink" to="steps">O co chodzi?</LinkScroll>
-                    <LinkScroll smooth={true} className="lowerMenuLink" to="about">O nas</LinkScroll>
-                    <LinkScroll smooth={true} className="lowerMenuLink" to="beneficiary">Fundacja i organizacje</LinkScroll>
-                    <LinkScroll smooth={true} className="lowerMenuLink" to="contact">Kontakt</LinkScroll>
-                </> :
-                <>
-                    <Link className="lowerMenuLink lowerMenuLink--active" to="/">Start</Link>
-                    <Link className="lowerMenuLink" to="/#steps">O co chodzi?</Link>
-                    <Link className="lowerMenuLink" to="/">O nas</Link>
-                    <Link className="lowerMenuLink" to="/">Fundacja i organizacje</Link>
-                    <Link className="lowerMenuLink" to="/">Kontakt</Link>
-                </>
-            }
+            <div onClick={()=>{changeLocation()}} className="lowerMenuLink lowerMenuLink--active" >Start</div>
+            <div onClick={()=>{changeLocation("steps")}}  className="lowerMenuLink" >O co chodzi?</div>
+            <div onClick={()=>{changeLocation("about")}}  className="lowerMenuLink">O nas</div>
+            <div onClick={()=>{changeLocation("beneficiary")}}  className="lowerMenuLink" >Fundacja i organizacje</div>
+            <div onClick={()=>{changeLocation("contact")}}  className="lowerMenuLink" >Kontakt</div>
         </div>
     );
 };
 
-export default LowerMenu;
+export default withRouter(LowerMenu);
